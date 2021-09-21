d86d97e76327ad01d837093a142f4eb77d08d375
nguyenanhung <info@nguyenanhung.com>
2021-09-21 23:31:36 +0700

    Add Monolog

---
+++ src/Logging.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Project basic-firewall
+ * Created by PhpStorm
+ * User: 713uk13m <dev@nguyenanhung.com>
+ * Copyright: 713uk13m <dev@nguyenanhung.com>
+ * Date: 09/21/2021
+ * Time: 23:27
+ */
+
+namespace nguyenanhung\PhpBasicFirewall;
+
+/**
+ * Class Logging - Thực hiện việc ghi và lưu trữ log, phân tích log
+ *
+ * Sử dụng gói Monolog để thực thi các tác vụ ghi log liên quan
+ *
+ * @package   nguyenanhung\PhpBasicFirewall
+ * @author    713uk13m <dev@nguyenanhung.com>
+ * @copyright 713uk13m <dev@nguyenanhung.com>
+ */
+class Logging
+{
+
+}
