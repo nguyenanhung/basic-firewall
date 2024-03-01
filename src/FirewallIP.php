4c1b56aadf552421b3894c9e0f6823086e9c8ba5
nguyenanhung <info@nguyenanhung.com>
2024-03-01 21:59:54 +0700

    Release v3.0.3

--- src/FirewallIP.php
+++ src/FirewallIP.php
@@ -19,272 +19,272 @@ use nguyenanhung\Component\Firewall\Firewall;
  */
 class FirewallIP
 {
-    /** @var string $logDestination Log File */
-    protected $logDestination;
+	/** @var string $logDestination Log File */
+	protected $logDestination;
 
-    /**
-     * @var array Cấu hình các IP được phép truy cập vào hệ thống
-     */
-    protected $ipWhiteList = array('127.0.0.1');
+	/**
+	 * @var array Cấu hình các IP được phép truy cập vào hệ thống
+	 */
+	protected $ipWhiteList = array('127.0.0.1');
 
-    /**
-     * @var array Cấu hình các IP không được phép truy cập vào hệ thống
-     */
-    protected $ipBlacklist = array();
+	/**
+	 * @var array Cấu hình các IP không được phép truy cập vào hệ thống
+	 */
+	protected $ipBlacklist = array();
 
-    /** @var bool Access Roles */
-    protected $access = false;
+	/** @var bool Access Roles */
+	protected $access = false;
 
-    /** @var bool Bypass if Use CLI */
-    protected $cliBypass = false;
+	/** @var bool Bypass if Use CLI */
+	protected $cliBypass = false;
 
-    /**
-     * Function setCliBypass
-     *
-     * @param false $cliBypass
-     *
-     * @return $this
-     * @author   : 713uk13m <dev@nguyenanhung.com>
-     * @copyright: 713uk13m <dev@nguyenanhung.com>
-     * @time     : 09/18/2021 09:28
-     */
-    public function setCliBypass(bool $cliBypass = false): FirewallIP
-    {
-        $this->cliBypass = $cliBypass;
+	/**
+	 * Function setCliBypass
+	 *
+	 * @param false $cliBypass
+	 *
+	 * @return $this
+	 * @author   : 713uk13m <dev@nguyenanhung.com>
+	 * @copyright: 713uk13m <dev@nguyenanhung.com>
+	 * @time     : 09/18/2021 09:28
+	 */
+	public function setCliBypass(bool $cliBypass = false): FirewallIP
+	{
+		$this->cliBypass = $cliBypass;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Function isCliBypass
-     *
-     * @return bool
-     * @author   : 713uk13m <dev@nguyenanhung.com>
-     * @copyright: 713uk13m <dev@nguyenanhung.com>
-     * @time     : 09/18/2021 13:47
-     */
-    public function isCliBypass(): bool
-    {
-        return $this->cliBypass;
-    }
+	/**
+	 * Function isCliBypass
+	 *
+	 * @return bool
+	 * @author   : 713uk13m <dev@nguyenanhung.com>
+	 * @copyright: 713uk13m <dev@nguyenanhung.com>
+	 * @time     : 09/18/2021 13:47
+	 */
+	public function isCliBypass(): bool
+	{
+		return $this->cliBypass;
+	}
 
-    /**
-     * Function isCLI
-     *
-     * @return bool
-     * @author   : 713uk13m <dev@nguyenanhung.com>
-     * @copyright: 713uk13m <dev@nguyenanhung.com>
-     * @time     : 09/18/2021 14:09
-     */
-    public function isCLI(): bool
-    {
-        return CheckSystem::isCLI();
-    }
+	/**
+	 * Function isCLI
+	 *
+	 * @return bool
+	 * @author   : 713uk13m <dev@nguyenanhung.com>
+	 * @copyright: 713uk13m <dev@nguyenanhung.com>
+	 * @time     : 09/18/2021 14:09
+	 */
+	public function isCLI(): bool
+	{
+		return CheckSystem::isCLI();
+	}
 
-    /**
-     * Function setLogDestination
-     *
-     * @param string $logDestination
-     *
-     * @return $this
-     * @author   : 713uk13m <dev@nguyenanhung.com>
-     * @copyright: 713uk13m <dev@nguyenanhung.com>
-     * @time     : 09/01/2021 50:21
-     */
-    public function setLogDestination(string $logDestination = ''): FirewallIP
-    {
-        $this->logDestination = $logDestination;
+	/**
+	 * Function setLogDestination
+	 *
+	 * @param string $logDestination
+	 *
+	 * @return $this
+	 * @author   : 713uk13m <dev@nguyenanhung.com>
+	 * @copyright: 713uk13m <dev@nguyenanhung.com>
+	 * @time     : 09/01/2021 50:21
+	 */
+	public function setLogDestination(string $logDestination = ''): FirewallIP
+	{
+		$this->logDestination = $logDestination;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Function setIpWhiteList
-     *
-     * @param array $ipWhiteList
-     *
-     * @return $this
-     * @author   : 713uk13m <dev@nguyenanhung.com>
-     * @copyright: 713uk13m <dev@nguyenanhung.com>
-     * @time     : 09/18/2021 29:42
-     */
-    public function setIpWhiteList(array $ipWhiteList = array()): FirewallIP
-    {
-        $this->ipWhiteList = $ipWhiteList;
+	/**
+	 * Function setIpWhiteList
+	 *
+	 * @param array $ipWhiteList
+	 *
+	 * @return $this
+	 * @author   : 713uk13m <dev@nguyenanhung.com>
+	 * @copyright: 713uk13m <dev@nguyenanhung.com>
+	 * @time     : 09/18/2021 29:42
+	 */
+	public function setIpWhiteList(array $ipWhiteList = array()): FirewallIP
+	{
+		$this->ipWhiteList = $ipWhiteList;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Function setIpBlackList
-     *
-     * @param array $ipBlacklist
-     *
-     * @return $this
-     * @author   : 713uk13m <dev@nguyenanhung.com>
-     * @copyright: 713uk13m <dev@nguyenanhung.com>
-     * @time     : 09/18/2021 29:46
-     */
-    public function setIpBlackList(array $ipBlacklist = array()): FirewallIP
-    {
-        $this->ipBlacklist = $ipBlacklist;
+	/**
+	 * Function setIpBlackList
+	 *
+	 * @param array $ipBlacklist
+	 *
+	 * @return $this
+	 * @author   : 713uk13m <dev@nguyenanhung.com>
+	 * @copyright: 713uk13m <dev@nguyenanhung.com>
+	 * @time     : 09/18/2021 29:46
+	 */
+	public function setIpBlackList(array $ipBlacklist = array()): FirewallIP
+	{
+		$this->ipBlacklist = $ipBlacklist;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Function getIPAddress
-     *
-     * @author: 713uk13m <dev@nguyenanhung.com>
-     * @time  : 2019-01-10 16:24
-     *
-     * @param bool $convertToInteger
-     *
-     * @return bool|int|string
-     */
-    public function getIPAddress(bool $convertToInteger = false)
-    {
-        $IPKeys = array(
-            0 => 'HTTP_CF_CONNECTING_IP',
-            1 => 'HTTP_X_FORWARDED_FOR',
-            2 => 'HTTP_X_FORWARDED',
-            3 => 'HTTP_X_IPADDRESS',
-            4 => 'HTTP_X_CLUSTER_CLIENT_IP',
-            5 => 'HTTP_FORWARDED_FOR',
-            6 => 'HTTP_FORWARDED',
-            7 => 'HTTP_CLIENT_IP',
-            8 => 'HTTP_IP',
-            9 => 'REMOTE_ADDR'
-        );
-        foreach ($IPKeys as $key) {
-            if (array_key_exists($key, $_SERVER) === true) {
-                $ips = explode(',', $_SERVER[$key]);
-                foreach ($ips as $ip) {
-                    $ip = trim($ip);
-                    if ($convertToInteger === true) {
-                        return ip2long($ip);
-                    }
+	/**
+	 * Function getIPAddress
+	 *
+	 * @param bool $convertToInteger
+	 *
+	 * @return bool|int|string
+	 * @author: 713uk13m <dev@nguyenanhung.com>
+	 * @time  : 2019-01-10 16:24
+	 *
+	 */
+	public function getIPAddress(bool $convertToInteger = false)
+	{
+		$IPKeys = array(
+			0 => 'HTTP_CF_CONNECTING_IP',
+			1 => 'HTTP_X_FORWARDED_FOR',
+			2 => 'HTTP_X_FORWARDED',
+			3 => 'HTTP_X_IPADDRESS',
+			4 => 'HTTP_X_CLUSTER_CLIENT_IP',
+			5 => 'HTTP_FORWARDED_FOR',
+			6 => 'HTTP_FORWARDED',
+			7 => 'HTTP_CLIENT_IP',
+			8 => 'HTTP_IP',
+			9 => 'REMOTE_ADDR'
+		);
+		foreach ($IPKeys as $key) {
+			if (array_key_exists($key, $_SERVER) === true) {
+				$ips = explode(',', $_SERVER[$key]);
+				foreach ($ips as $ip) {
+					$ip = trim($ip);
+					if ($convertToInteger === true) {
+						return ip2long($ip);
+					}
 
-                    return $ip;
-                }
-            }
-        }
+					return $ip;
+				}
+			}
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    /**
-     * Function errorLogMessage
-     *
-     * @author: 713uk13m <dev@nguyenanhung.com>
-     * @time  : 2019-01-10 16:29
-     *
-     * @return array
-     */
-    public function errorLogMessage(): array
-    {
-        $context = array(
-            'IP'           => $this->getIPAddress(),
-            'COUNTRY'      => $_SERVER['HTTP_CF_IPCOUNTRY'] ?? '',
-            'PROTOCOL'     => $_SERVER['HTTP_X_FORWARDED_PROTO'] ?? '',
-            'METHOD'       => $_SERVER['REQUEST_METHOD'] ?? '',
-            'HOST'         => $_SERVER['HTTP_HOST'] ?? '',
-            'URI'          => $_SERVER['REQUEST_URI'] ?? '',
-            'QUERY_STRING' => $_SERVER['QUERY_STRING'] ?? '',
-            'USER_AGENT'   => $_SERVER['HTTP_USER_AGENT'] ?? '',
-        );
-        $message = $this->getIPAddress() . ' -> Access Denied!';
+	/**
+	 * Function errorLogMessage
+	 *
+	 * @return array
+	 * @author: 713uk13m <dev@nguyenanhung.com>
+	 * @time  : 2019-01-10 16:29
+	 *
+	 */
+	public function errorLogMessage(): array
+	{
+		$context = array(
+			'IP' => $this->getIPAddress(),
+			'COUNTRY' => $_SERVER['HTTP_CF_IPCOUNTRY'] ?? '',
+			'PROTOCOL' => $_SERVER['HTTP_X_FORWARDED_PROTO'] ?? '',
+			'METHOD' => $_SERVER['REQUEST_METHOD'] ?? '',
+			'HOST' => $_SERVER['HTTP_HOST'] ?? '',
+			'URI' => $_SERVER['REQUEST_URI'] ?? '',
+			'QUERY_STRING' => $_SERVER['QUERY_STRING'] ?? '',
+			'USER_AGENT' => $_SERVER['HTTP_USER_AGENT'] ?? '',
+		);
+		$message = $this->getIPAddress() . ' -> Access Denied!';
 
-        return array(
-            'message' => $message,
-            'context' => $context
-        );
-    }
+		return array(
+			'message' => $message,
+			'context' => $context
+		);
+	}
 
-    /**
-     * Function accessDenied
-     *
-     * @author: 713uk13m <dev@nguyenanhung.com>
-     * @time  : 2019-01-10 16:29
-     *
-     * @return string
-     */
-    public function accessDenied(): string
-    {
-        return 'Access Denied!';
-    }
+	/**
+	 * Function accessDenied
+	 *
+	 * @return string
+	 * @author: 713uk13m <dev@nguyenanhung.com>
+	 * @time  : 2019-01-10 16:29
+	 *
+	 */
+	public function accessDenied(): string
+	{
+		return 'Access Denied!';
+	}
 
-    /**
-     * Function accessDeniedResponse
-     *
-     * @author   : 713uk13m <dev@nguyenanhung.com>
-     * @copyright: 713uk13m <dev@nguyenanhung.com>
-     * @time     : 09/18/2021 41:56
-     */
-    public function accessDeniedResponse()
-    {
-        http_response_code(403);
-        exit($this->accessDenied());
-    }
+	/**
+	 * Function accessDeniedResponse
+	 *
+	 * @author   : 713uk13m <dev@nguyenanhung.com>
+	 * @copyright: 713uk13m <dev@nguyenanhung.com>
+	 * @time     : 09/18/2021 41:56
+	 */
+	public function accessDeniedResponse()
+	{
+		http_response_code(403);
+		exit($this->accessDenied());
+	}
 
-    /**
-     * Function writeErrorLog
-     *
-     * @param array $data
-     *
-     * @author   : 713uk13m <dev@nguyenanhung.com>
-     * @copyright: 713uk13m <dev@nguyenanhung.com>
-     * @time     : 09/24/2021 13:31
-     */
-    public function writeErrorLog(array $data = array())
-    {
-        $log = new Logging();
-        $log->setLogDestination($this->logDestination)->write($data['message'], $data['context']);
-    }
+	/**
+	 * Function writeErrorLog
+	 *
+	 * @param array $data
+	 *
+	 * @author   : 713uk13m <dev@nguyenanhung.com>
+	 * @copyright: 713uk13m <dev@nguyenanhung.com>
+	 * @time     : 09/24/2021 13:31
+	 */
+	public function writeErrorLog(array $data = array())
+	{
+		$log = new Logging();
+		$log->setLogDestination($this->logDestination)->write($data['message'], $data['context']);
+	}
 
-    /**
-     * Function checkUserConnect
-     *
-     * @param bool $defaultState
-     *
-     * @return $this
-     * @author   : 713uk13m <dev@nguyenanhung.com>
-     * @copyright: 713uk13m <dev@nguyenanhung.com>
-     * @time     : 09/18/2021 59:34
-     */
-    public function checkUserConnect(bool $defaultState = false): FirewallIP
-    {
-        $firewall = new Firewall();
-        $firewall->setDefaultState($defaultState);
-        if (defined('HUNGNG_IP_WHITELIST') && is_array(HUNGNG_IP_WHITELIST)) {
-            $firewall->addList(HUNGNG_IP_WHITELIST, 'local', true);
-        } elseif (!empty($this->ipWhiteList)) {
-            $firewall->addList($this->ipWhiteList, 'local', true);
-        }
-        if (defined('HUNGNG_IP_BLACKLIST') && is_array(HUNGNG_IP_BLACKLIST)) {
-            $firewall->addList(HUNGNG_IP_BLACKLIST, 'localBad', false);
-        } elseif (!empty($this->ipBlacklist)) {
-            $firewall->addList($this->ipBlacklist, 'localBad', false);
-        }
-        $this->access = $firewall->setIpAddress($this->getIPAddress())->handle();
+	/**
+	 * Function checkUserConnect
+	 *
+	 * @param bool $defaultState
+	 *
+	 * @return $this
+	 * @author   : 713uk13m <dev@nguyenanhung.com>
+	 * @copyright: 713uk13m <dev@nguyenanhung.com>
+	 * @time     : 09/18/2021 59:34
+	 */
+	public function checkUserConnect(bool $defaultState = false): FirewallIP
+	{
+		$firewall = new Firewall();
+		$firewall->setDefaultState($defaultState);
+		if (defined('HUNGNG_IP_WHITELIST') && is_array(HUNGNG_IP_WHITELIST)) {
+			$firewall->addList(HUNGNG_IP_WHITELIST, 'local', true);
+		} elseif (!empty($this->ipWhiteList)) {
+			$firewall->addList($this->ipWhiteList, 'local', true);
+		}
+		if (defined('HUNGNG_IP_BLACKLIST') && is_array(HUNGNG_IP_BLACKLIST)) {
+			$firewall->addList(HUNGNG_IP_BLACKLIST, 'localBad', false);
+		} elseif (!empty($this->ipBlacklist)) {
+			$firewall->addList($this->ipBlacklist, 'localBad', false);
+		}
+		$this->access = $firewall->setIpAddress($this->getIPAddress())->handle();
 
-        // Nếu chạy từ CLI -> bypass
+		// Nếu chạy từ CLI -> bypass
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Function isAccess
-     *
-     * @return bool
-     * @author   : 713uk13m <dev@nguyenanhung.com>
-     * @copyright: 713uk13m <dev@nguyenanhung.com>
-     * @time     : 09/18/2021 41:24
-     */
-    public function isAccess(): bool
-    {
-        return $this->access;
-    }
+	/**
+	 * Function isAccess
+	 *
+	 * @return bool
+	 * @author   : 713uk13m <dev@nguyenanhung.com>
+	 * @copyright: 713uk13m <dev@nguyenanhung.com>
+	 * @time     : 09/18/2021 41:24
+	 */
+	public function isAccess(): bool
+	{
+		return $this->access;
+	}
 
 }
 
